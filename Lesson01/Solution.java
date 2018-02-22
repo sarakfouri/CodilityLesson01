8adc3a22bbabde06d3cdf57fd8a35e2e7a6db1c7
Sara Kfouri <sakfouri@gmail.com>
2018-02-22 22:17:40 +0000

    Update Solution.java

--- Lesson01/Solution.java
+++ Lesson01/Solution.java
@@ -1,4 +1,6 @@
 public class Solution {
+	//88% 
+	public int solution (int N){
 String numbers = Integer.toBinaryString(N);
 	int count = 0;
 	int big = 0;
@@ -20,4 +22,30 @@ String numbers = Integer.toBinaryString(N);
 	}
 	
 	return  Math.max(big, count);
-}
+
+// the best 100 %
+	
+public int solution(int N){
+	    	    String binaryNumbers = Integer.toBinaryString(N);
+	    	    char [] numbers = binaryNumbers.toCharArray();
+	    	    int count = 0;
+	    	    int big = 0;
+	    	    for (int i = 0; i < numbers.length; i++) {
+	    	        if(numbers[i] == '1') {
+	    	            if (count > big) { 
+	    	            	big = count; 
+	    	            	}
+	    	            count = 0;
+	    	        }else{
+	    	        	count++; 
+	    	        	}
+	    	    }
+	    	    return big;
+	    	}
+		
+		
+	}
+		
+		
+		
+		
