9e2e43bb9fd1ad4d5dcb9654028dabcbf84c9018
Sara Kfouri <sakfouri@gmail.com>
2018-02-22 21:15:19 +0000

    Update Solution.java

--- Lesson01/Solution.java
+++ Lesson01/Solution.java
@@ -1,17 +1,11 @@
 public class Solution {
-public int solution(int N){
-	// converts Integer to binary and transforms into a String
-	String m = Integer.toBinaryString(N);
-	// test if the variable transform into a binary/string
-	System.out.println(m);
-	char[] numbers = m.toCharArray();
-	// initializes the two variables to count the repeated zero sequences
+String numbers = Integer.toBinaryString(N);
 	int count = 0;
 	int big = 0;
 	// 
-	for (int i = 0; i < numbers.length; i++){
+	for (int i = 0; i < numbers.length(); i++){
 		
-		if (numbers[i]=='0'){
+		if (numbers.charAt(i) == '0'){
 			++count;
 			
 			}else{
@@ -25,6 +19,5 @@ public int solution(int N){
 		}
 	}
 	
-	return big;
-}
+	return  Math.max(big, count);
 }
