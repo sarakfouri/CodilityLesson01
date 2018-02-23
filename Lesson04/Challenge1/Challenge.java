4a4f86c0078fd321423d3ce8102dae319cd180eb
Sara Kfouri <sakfouri@gmail.com>
2018-02-23 22:10:39 +0000

    Update Challenge.java

--- Lesson04/Challenge1/Challenge.java
+++ Lesson04/Challenge1/Challenge.java
@@ -45,4 +45,34 @@ class Solution {
     }
 }
 
-//
+//100%
+import java.util.Arrays;
+
+class Solution {
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+//For example, for the input [3, 1] the solution returned a wrong answer (got 1 expected 0).
+    public int solution(int[] A) {
+     Arrays.sort(A);
+     int number = 0;
+     if (A.length <= 1){
+         if(A[0] == 1 ){
+             return 1;
+         }
+         return 0;
+     }
+     int init = A[0];
+     if (A.length == 2 && A[0] != 1 ){
+         return 0;
+     }
+     for (int i = 1; i < A.length; i++){
+         if (A[i] == (init+1)){
+                number = 1; 
+                init = A[i];
+         }else{
+             number = 0;
+             return number;
+         }
+     }return number;
+    }
+}
