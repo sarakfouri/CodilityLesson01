fa1664da5a139439caa0ce9abdee4d14eec183cd
Sara Kfouri <sakfouri@gmail.com>
2018-02-23 20:53:08 +0000

    Update Challenge.java

--- Lesson03/Challenge3/Challenge.java
+++ Lesson03/Challenge3/Challenge.java
@@ -100,3 +100,45 @@ class Solution {
     }return number;
     }
 }
+import java.util.Arrays;
+ class Solution {
+    
+    public int solution(int[] A) {
+     Arrays.sort(A);
+    int number = 0;
+      if (A.length <= 1){
+          if(A.length == 1){
+              if(A[0] != 1 ){
+                return A[0] - 1;          
+              }
+                   else{
+                      return A[0] + 1; 
+                   }
+      }else{
+          return 1;
+      }
+      }
+      
+      if(A[0] > 1){
+          return A[0] -1;
+      }    
+        int ini = A[0];
+      for (int i = 1; i < A.length; i++){
+          if (A[i] != ini+1){
+              number = ini +1;
+              return number;
+          }else {
+             ini = A[i];
+             if (i == A.length - 1){
+                 if(A[0] == 1){
+                     number = A[i] + 1;
+                 }else{
+                        number = A[0] -1;                 
+             }  
+             }
+        }
+
+          
+      }return number;
+      }
+ }
